6c7fa6b1a489e4d4e66c9e038c57dc4331f1d03e
Alexander DuPree <alexander.j.dupree@gmail.com>
2019-05-08 16:31:19 -0700

    Added commandNotFound handler

--- Ash/Core/Executor.hs
+++ Ash/Core/Executor.hs
@@ -6,14 +6,19 @@
 -- Stability   :  experimental
 -- Portability :  POSIX
 
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Core.Executor
     (execute
     ) where
 
 import           BuiltIns.Table
+import           Control.Exception (IOException, handle)
 import qualified Data.Text         as T
+import qualified Data.Text.IO      as I
 import           GHC.IO.Handle     (Handle)
-import           System.Exit       (ExitCode)
+import           System.Exit       (ExitCode (..))
 import           System.Process
     ( ProcessHandle
     , createProcess
@@ -23,14 +28,20 @@ import           System.Process
     )
 
 execute :: [T.Text] -> IO ExitCode
-execute argv = case searchBuiltIns command of
-                 Just cmd -> cmd args
-                 Nothing  -> execute' (T.unpack command) $ map T.unpack args 
-                 where command = head argv
-                       args    = tail argv
+execute argv = handle (\ (e ::IOException) -> commandNotFound command ) $
+    case searchBuiltIns command of
+        Just cmd -> cmd args
+        Nothing  -> execute' (T.unpack command) $ map T.unpack args
+        where command = head argv
+              args    = tail argv
 
 execute' :: String -> [String] -> IO ExitCode
-execute' command args = createProcess (proc command args){ delegate_ctlc = True } 
+execute' command args = createProcess (proc command args){ delegate_ctlc = True }
     >>= \thread -> waitForProcess . getHandle $ thread
     where getHandle (_, _, _, handle) = handle
+
+commandNotFound :: T.Text -> IO ExitCode
+commandNotFound command = do
+    I.putStrLn $ "ash: command not found: " `T.append` command
+    return ( ExitFailure 1 )
 
